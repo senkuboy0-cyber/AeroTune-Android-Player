25d303bd283f143c47ac605720af872f4da64991
senkuboy0-cyber <senkuboy0@gmail.com>
2026-04-30 22:46:09 +0600

    Consolidate DI modules - simplified setup

--- app/src/main/java/com/aerotune/player/di/AppModule.kt
+++ app/src/main/java/com/aerotune/player/di/AppModule.kt
@@ -1,24 +1,32 @@
 package com.aerotune.player.di
 
 import android.content.ContentResolver
+import android.content.Context
+import androidx.media3.exoplayer.ExoPlayer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
     @Provides
     @Singleton
     fun provideContentResolver(
-        resolver: AppContentResolver
-    ): ContentResolver = resolver.get()
-}
+        @ApplicationContext context: Context
+    ): ContentResolver {
+        return context.contentResolver
+    }
 
-class AppContentResolver @javax.inject.Inject constructor(
-    private val resolver: ContentResolver
-) {
-    fun get(): ContentResolver = resolver
+    @Provides
+    @Singleton
+    fun provideExoPlayer(
+        @ApplicationContext context: Context
+    ): ExoPlayer {
+        return ExoPlayer.Builder(context).build()
+    }
 }
