d878e89c85cc42e8f432c6efe03e7739397aa427
senkuboy0-cyber <senkuboy0@gmail.com>
2026-04-30 22:55:41 +0600

    Fix KSP version to 2.1.0-1.0.29

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
     id("com.google.dagger.hilt.android") version "2.56.1" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.30" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
 }
