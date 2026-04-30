50dbcb8161bf4474273d027836a23216a1a06316
senkuboy0-cyber <senkuboy0@gmail.com>
2026-04-30 22:39:06 +0600

    Fix gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,3 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
-
-include("composecompiler")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,10 +16,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "AeroTune"
 include(":app")
-
-// Setup Compose compiler build to allow local compose version overrides
-gradle.beforeProject {
-    if (project.name == "composecompiler") {
-        return@beforeProject
-    }
-}
