90d866c42843fb33015fb93df31b82a74e92a240
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-01 07:05:16 +0600

    Remove AndroidContentResolver.kt - duplicate code

--- app/src/main/java/com/aerotune/player/AndroidContentResolver.kt
+++
@@ -1,9 +0,0 @@
-package com.aerotune.player
-
-import android.content.ContentResolver
-
-class AndroidContentResolver(
-    private val contentResolver: ContentResolver
-) {
-    fun get(): ContentResolver = contentResolver
-}
